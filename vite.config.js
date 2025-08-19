619ab6bb8ee7f29fb49e9460a79e8bd409c9236b
bablukumar05 <kumarbablu.com@gmail>
2025-08-19 13:15:59 +0530

    Add deploy script and base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: '/Employee--Management-System/', // 👈 Add this line
 })
